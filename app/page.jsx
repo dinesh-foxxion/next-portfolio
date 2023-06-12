5a5d69eed51d8e2b2f6afba7e3e85a4c9a4710d5
dinesh-foxxion <dinesh@foxxion.com>
2023-06-12 12:39:54 +0530

    refact: reset entire project

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui voluptatum vero, recusandae, omnis commodi libero ab enim odit pariatur velit nihil assumenda ipsum sunt adipisci consequatur quod maxime eum.</p>
+    </div>
+  )
+}
+
+export default HomePage
